30c4bad60e974f006415bf3e49b89762048f9fd7
Juan Quino <quinojuan@gmail.com>
2023-08-29 00:30:53 -0300

    footer update

--- Footer.jsx
+++ Footer.jsx
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
         <div className="footer">
-            <p className="text-base">Desarrollado por Juan Quino - <span className="font-bold">Cel. 11 6030 0518</span></p>
+            <p className="text-base">Desarrollado por: Juan Quino - <span className="font-bold">Cel. 11 6030 0518</span></p>
+            <p className="mt-2 font-italic">Juan José Castelli - Chaco</p>
         </div>
     )
 }
