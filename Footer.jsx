1b3bdd5bcebc5f899b867c83c284b7228f4a435f
Juan Quino <quinojuan@gmail.com>
2023-11-14 18:33:50 -0300

    footer

--- Footer.jsx
+++ Footer.jsx
@@ -1,11 +1,14 @@
 const Footer = () => {
-    return (
-        <div className="footer">
-            <p className="text-base">Desarrollado por: Juan Quino - <span className="font-bold">Cel. 11 6030 0518</span></p>
-            <p className="mt-2 font-italic">Juan José Castelli - Chaco</p>
-            <p className="text-primary text-xs">Actualizado al 13/10/2023</p>
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <p className="text-base">
+        Desarrollado por: Juan Quino -{" "}
+        <span className="font-bold">Cel. 11 6030 0518</span>
+      </p>
+      <p className="mt-2 font-italic">Juan José Castelli - Chaco</p>
+      <p className="text-primary text-xs">Actualizado al 14/11/2023</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
