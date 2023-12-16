db3579ed2bd13ab011cc709af98181eee9974c11
quinojuan <quinojuan@gmail.com>
2023-12-16 12:58:32 -0300

    enviando el archivo excel a una ruta

--- Footer.jsx
+++ Footer.jsx
@@ -1,13 +1,19 @@
 const Footer = () => {
   return (
-    <div className="footer">
-      <p className="text-base">
-        Desarrollado por: Juan Quino -{" "}
-        <span className="font-bold">Cel. 11 6030 0518</span>
-      </p>
-      <p className="mt-2 font-italic">Juan José Castelli - Chaco</p>
-      <p className="text-primary text-xs">Actualizado al 14/11/2023</p>
-    </div>
+    <>
+      <div className="footer">
+        <p className="text-base">
+          Desarrollado por: Juan Quino -{" "}
+          <span className="font-bold">Cel. 11 6030 0518</span>
+        </p>
+        <p className="mt-2 font-italic">Juan José Castelli - Chaco</p>
+        <p className="text-primary text-xs">Actualizado al 14/11/2023</p>
+      </div>
+      <form action="http://localhost:3000/file" method="post" enctype="multipart/form-data">
+        <input type="file" name="archivoExcel"></input>
+        <input type="submit" value="Enviar"></input>
+      </form>
+    </>
   );
 };
 
